9340eab4cd60b8e9dfbf00e997667968a73437d8
eray-mutabesha <mutabeshaeray@gmail.com>
2023-06-07 17:39:02 +0200

    done with the back end

---
+++ index.js
@@ -0,0 +1,52 @@
+ const inpt=document.getElementById("task");
+ const btn_add=document.getElementById("add_btn");
+ const dive=document.getElementById("dive");
+ 
+
+ 
+
+var task="";
+inpt.addEventListener("input",(e)=>{
+   task=e.target.value;
+})
+ 
+   btn_add.addEventListener("click",(e)=>{
+      inpt.setCustomValidity("vous n'avez pas entrer de programme");
+      e.preventDefault();
+      const delet=document.createElement("button");
+      const li=document.createElement("li");
+      const chek=document.createElement("input");
+      const nav=document.createElement("nav");
+      nav.appendChild(delet);
+      delet.innerHTML="x";
+      nav.appendChild(li);
+      nav.appendChild(chek);
+      nav.style.display="flex";
+      chek.type="checkbox";
+    
+      li.style.background="green";
+      li.style.margin="5px";
+      li.style.width="30%";
+      li.style.listStyle="none";
+      dive.appendChild(nav);
+      chek.style.margin="5px";
+      dive.style.display="grid"; 
+      li.innerHTML=task;
+      delet.addEventListener("click",(e)=>{
+        nav.remove();
+      })
+
+      if (inpt.value=="") {
+        li.remove();
+        chek.remove();
+        const message=getElementById("message");
+        message.innerHTML="you need to add tasks to do";
+      }
+     })
+ 
+   
+const resetall=document.getElementById("clear_btn");
+resetall.addEventListener("click",(e)=>{
+   window.location.reload();
+   
+})
