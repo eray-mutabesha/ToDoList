5e612e36c96b842812afd73b8fec79b8256208f1
eray-mutabesha <mutabeshaeray@gmail.com>
2023-06-26 14:36:56 +0200

    changes

--- index.js
+++ index.js
@@ -1,40 +1,59 @@
  const inpt=document.getElementById("task");
  const btn_add=document.getElementById("add_btn");
  const dive=document.getElementById("dive");
-var task="";
-inpt.addEventListener("input",(e)=>{
+
+// 0000000000000000000000000000000000000000000000000000000000000000
+
+
+
+
+ var task="";
+ inpt.addEventListener("input",(e)=>{
    task=e.target.value;
 })
  
    btn_add.addEventListener("click",(e)=>{
+    const diveflex=document.createElement("div");
+    const para=document.createElement("div");
+
+
       inpt.setCustomValidity("vous n'avez pas entrer de programme");
       e.preventDefault();
-      const delet=document.createElement("button");
-      const li=document.createElement("li");
-      const chek=document.createElement("input");
-      const nav=document.createElement("nav");
-      delet.innerHTML="x";
-      nav.appendChild(delet);
-      nav.appendChild(li);
-      nav.appendChild(chek);
-      nav.style.display="flex";
-      chek.type="checkbox";
-      
-      li.style.margin="5px";
-      li.style.width="100%";
-      li.style.listStyle="none";
-      dive.appendChild(nav);
-      chek.style.margin="5px";
-      dive.style.display="grid"; 
-      li.innerHTML=task;
-      delet.addEventListener("click",(e)=>{
-        nav.remove();
-      })
 
+      const delet=document.createElement("button");
+      const li=document.createElement("p");
+      const chek=document.createElement("input");
+
+      delet.innerHTML="x";
+      chek.type="checkbox";
+      if(chek.checked="true"){
+        alert("hello");
+       }
+
+      diveflex.appendChild(chek);
+      chek.style.borderRadius="50%";
+      diveflex.appendChild(para);
+      para.appendChild(li);
+     
+      li.style.listStyle="none";
+      li.innerHTML=task;
+      diveflex.appendChild(delet);
+
+      dive.appendChild(diveflex);
+      diveflex.style.display="flex";
+      diveflex.style.justifyContent="";
+      diveflex.style.padding="10px";
+      dive.style.display="grid";
+      
+      
+      delet.addEventListener("click",(e)=>{
+        diveflex.remove();
+      })
       if (inpt.value=="") {
         li.remove();
         chek.remove();
         delet.remove();
+        diveflex.remove();
         const message=getElementById("message");
         message.innerHTML="you need to add tasks to do";
       }
