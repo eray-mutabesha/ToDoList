9235b0f0a632bca64dd1a99a5c04cba5ae08773b
Francis Hakim BC <francishakimbc@Franciss-MacBook-Air.local>
2023-08-29 16:49:07 +0200

    localstorage

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 
-let inpt=document.getElementById('task');
+const inpt=document.getElementById('task');
+const liste=document.getElementById('liste');
+
+function addtask(){
+  if(inpt.value === ""){
+    alert('you must add a task');
+  }
+ else{
+  let li = document.createElement('li');
+  li.innerHTML = inpt.value;
+  liste.appendChild(li);
+  let span=document.createElement('span');
+  span.innerHTML="\u00d7";
+  li.appendChild(span);  
+ }
+ inpt.value=""; 
+ setdata();
+}
+
+liste.addEventListener("click",function(e){
+  if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked");
+    setdata();
+  }
+  else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+    setdata();
+  }
+},false);
+
+function setdata(){
+  localStorage.setItem('donnees',liste.innerHTML);
+}
+function getdata(){
+    liste.innerHTML=localStorage.getItem('donnees');
+}
+getdata();
